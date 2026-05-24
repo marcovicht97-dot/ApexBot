e0b70c5fc463b04382eaf62eda158671e70f0576
marcovicht97-dot <marcovich.t97@gmail.com>
2026-05-24 02:26:54 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+
+const { Client, GatewayIntentBits } = require('discord.js');
+const axios = require('axios');
+
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds]
+});
+
+const CHANNEL_ID = '1507856465803874336';
+
+async function sendMapRotation() {
+    try {
+
+        const response = await axios.get(
+            'https://api.mozambiquehe.re/maprotation?auth=d1e8a7766be6b62ade1c00a2941bc2b3&version=2'
+        );
+
+        const rankedMap = response.data.ranked.current.map;
+        const remaining = response.data.ranked.current.remainingTimer;
+
+        const channel = await client.channels.fetch(CHANNEL_ID);
+
+        channel.send(
+            `🎮 Ranked mapa: **${rankedMap}**\n⏳ Zbývá: ${remaining}`
+        );
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+client.once('ready', () => {
+    console.log(`Přihlášen jako ${client.user.tag}`);
+
+    sendMapRotation();
+
+    setInterval(sendMapRotation, 60 * 60 * 1000);
+});
+
+client.login(process.env.TOKEN);
